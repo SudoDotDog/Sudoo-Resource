52de6680add295eebeeb45feea8ee174c2810557
WMXPY <wm@mengw.io>
2021-07-10 15:14:04 -0500

    move handler

--- src/handler/handler.ts
+++ src/handler/handler.ts
@@ -4,7 +4,7 @@
  * @description Handler
  */
 
-import { ResourceNamespace } from "./namespace/namespace";
+import { ResourceNamespace } from "../namespace/namespace";
 
 export class ResourceHandler {
 
