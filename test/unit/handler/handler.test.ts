d23e1123cc75434c48dac46a71cc7fe9328478cd
WMXPY <wm@mengw.io>
2021-07-18 01:31:41 -0500

    add handler test

---
+++ test/unit/handler/handler.test.ts
@@ -0,0 +1,32 @@
+/**
+ * @author WMXPY
+ * @namespace Resource_Handler
+ * @description Handler
+ * @override Unit Test
+ */
+
+import { expect } from "chai";
+import * as Chance from "chance";
+import { ResourceCategory, ResourceSubset } from "../../../src";
+import { ResourceHandler } from "../../../src/handler/handler";
+import { ResourceNamespace } from "../../../src/namespace/namespace";
+
+describe('Given {ResourceHandler} Class', (): void => {
+
+    const chance: Chance.Chance = new Chance('resource-handler-handler');
+
+    it('should be able to construct', (): void => {
+
+        const namespace: ResourceNamespace = ResourceNamespace.create(chance.word());
+        const category: ResourceCategory = ResourceCategory.fromSubsets(
+            chance.word(),
+            ResourceSubset.identifier(chance.word()),
+        );
+
+        namespace.addCategory(category);
+
+        const handler: ResourceHandler = ResourceHandler.fromNamespace(namespace);
+
+        expect(handler).to.be.instanceOf(ResourceHandler);
+    });
+});
