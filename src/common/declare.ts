6593e3ba16a10ac900a2fff58fd70da52e036670
WMXPY <wm@mengw.io>
2021-10-24 15:24:12 -0500

    add producer

--- src/common/declare.ts
+++ src/common/declare.ts
@@ -7,5 +7,6 @@
 export const RESOURCE_CATEGORY_NAME_SEPARATOR = '^';
 export const RESOURCE_NAMESPACE_SEPARATOR = '::';
 export const RESOURCE_CATEGORY_SEPARATOR = ':';
+export const RESOURCE_SUBSET_SEPARATOR = ':';
 export const UNIFORM_RESOURCE_NAME_NAMESPACE = 'urn';
 export const SUDO_RESOURCE_NAME_NAMESPACE = 'sudo';
--- src/producer/producer.ts
+++ src/producer/producer.ts
@@ -5,6 +5,8 @@
  */
 
 import { ResourceCategory } from "../category/category";
+import { RESOURCE_SUBSET_SEPARATOR } from "../common/declare";
+import { IResourceSubset } from "../subset/declare";
 
 export class ResourceProducer {
 
@@ -18,5 +20,18 @@ export class ResourceProducer {
     private constructor(category: ResourceCategory) {
 
         this._category = category;
+    }
+
+    public produceByRecord(record: Record<string, string>): string {
+
+        const subsets: IResourceSubset[] = this._category.subsets;
+        const resultList: string[] = [];
+
+        for (const subset of subsets) {
+            if (typeof record[subset.subsetName] !== 'undefined') {
+                resultList.push(record[subset.subsetName]);
+            }
+        }
+        return resultList.join(RESOURCE_SUBSET_SEPARATOR);
     }
 }
